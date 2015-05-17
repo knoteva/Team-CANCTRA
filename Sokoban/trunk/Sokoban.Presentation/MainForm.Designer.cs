f5321f571c81c9cb3707558dd359c35b13988c7a
kitsune0408 <kitsune0408@yahoo.com>
2015-05-17 13:39:02 +0300

    new try

---
+++ Sokoban/trunk/Sokoban.Presentation/MainForm.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace Sokoban.Presentation
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.startToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.levelSelectionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.restartButton = new System.Windows.Forms.ToolStripButton();
+            this.undoButton = new System.Windows.Forms.ToolStripButton();
+            this.drawingArea = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.drawingArea)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panel1.Controls.Add(this.drawingArea);
+            this.panel1.Location = new System.Drawing.Point(13, 52);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(612, 322);
+            this.panel1.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.startToolStripMenuItem,
+            this.levelSelectionToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(637, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // startToolStripMenuItem
+            // 
+            this.startToolStripMenuItem.Name = "startToolStripMenuItem";
+            this.startToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.startToolStripMenuItem.Text = "Start";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.restartButton,
+            this.undoButton});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 24);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(637, 25);
+            this.toolStrip1.TabIndex = 2;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // levelSelectionToolStripMenuItem
+            // 
+            this.levelSelectionToolStripMenuItem.Name = "levelSelectionToolStripMenuItem";
+            this.levelSelectionToolStripMenuItem.Size = new System.Drawing.Size(96, 20);
+            this.levelSelectionToolStripMenuItem.Text = "Level selection";
+            this.levelSelectionToolStripMenuItem.Click += new System.EventHandler(this.levelSelectionToolStripMenuItem_Click);
+            // 
+            // restartButton
+            // 
+            this.restartButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.restartButton.Image = global::Sokoban.Presentation.Properties.Resources.restart;
+            this.restartButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.restartButton.Name = "restartButton";
+            this.restartButton.Size = new System.Drawing.Size(23, 22);
+            this.restartButton.Text = "restartButton";
+            // 
+            // undoButton
+            // 
+            this.undoButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.undoButton.Image = global::Sokoban.Presentation.Properties.Resources.undo;
+            this.undoButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.undoButton.Name = "undoButton";
+            this.undoButton.Size = new System.Drawing.Size(23, 22);
+            this.undoButton.Text = "undoButton";
+            // 
+            // drawingArea
+            // 
+            this.drawingArea.BackColor = System.Drawing.Color.Gray;
+            this.drawingArea.Location = new System.Drawing.Point(157, 31);
+            this.drawingArea.Name = "drawingArea";
+            this.drawingArea.Size = new System.Drawing.Size(302, 259);
+            this.drawingArea.TabIndex = 0;
+            this.drawingArea.TabStop = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(637, 386);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.Text = "Socoban";
+            this.panel1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.drawingArea)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox drawingArea;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem startToolStripMenuItem;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton restartButton;
+        private System.Windows.Forms.ToolStripButton undoButton;
+        private System.Windows.Forms.ToolStripMenuItem levelSelectionToolStripMenuItem;
+    }
+}
+
---
+++ Sokoban/trunk/Sokoban.Presentation/MainForm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sokoban.Presentation
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void GoToLevel(int levelNumber)
+        {
+
+        }
+
+        private void levelSelectionToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LevelSelectionForm levelSelectionForm = new LevelSelectionForm();
+            if (levelSelectionForm.ShowDialog() == DialogResult.OK)
+            {
+               
+            }
+        }
+    }
+}
