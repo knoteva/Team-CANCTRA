a081f3317fa99d642f2be7a9f5e543d0ee48b68b
Katerina Noteva <knoteva@gmail.com>
2015-05-19 11:03:29 +0300

    върнат level

---
+++ Sokoban.Logic/Level.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sokoban.Logic
+{
+    public class Level
+    {
+        public string[] Data { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+    }
+}
+
